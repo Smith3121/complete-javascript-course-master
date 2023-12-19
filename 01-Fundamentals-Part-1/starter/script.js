66b209396e1c00c4f391cb4446cacd7b0162e860
Kovács Ákos <kovacsakos961@gmail.com>
2023-12-19 20:41:04 +0200

    lecture 24 and 25

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -381,7 +381,7 @@ winnerTeam(dolphinsTwo, koalasTwo);
 winnerTeam(dolphinsThree, koalasThree);
  */
 
-const day = "saturday";
+/* const day = "saturday";
 
 switch (day) {
   case "monday": // "day" === "monday"
@@ -420,3 +420,56 @@ if (day === "monday") {
 } else {
   console.log("Not a valid day!");
 }
+ */
+
+//lecture 24: Statements and expressions
+/* 3 + 4;
+1991;
+true && false && !false;
+
+if (23 > 10) {
+  const str = "23 is bigger";
+}
+
+const me = "Jonas";
+console.log(`I'am ${2037 - 1991} years old ${me}`);
+ */
+
+//lecture 25: The Conditional (Ternary) operators
+
+/* const age = 23;
+age >= 18
+  ? console.log("I like to drink wine")
+  : console.log("I like to dtink water");
+
+const drink = age >= 18 ? "wine" : "water";
+console.log(drink);
+
+let drink2;
+if (age >= "wine") {
+  drink2 = "wine";
+} else {
+  drink2 = "water";
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
+ */
+
+/* Coding Challenge #4
+Steven wants to build a very simple tip calculator for whenever he goes eating in a
+restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and
+300. If the value is different, the tip is 20%.
+Your tasks:
+1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for
+this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can
+start with an if/else statement, and then try to convert it to a ternary
+operator!)
+2. Print a string to the console containing the bill value, the tip, and the final value
+(bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value
+316.25”
+Test data:
+§ Data 1: Test for bill values 275, 40 and 430
+Hints:
+§ To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+§ Value X is between 50 and 300, if it's >= 50 && <= 300 😉 */
