4ed6cc8dd240cf0868ae907a3bb1d7970e532762
smith1311 <kovacsakos961@gmail.com>
2023-12-21 21:22:11 +0200

    lecture 29: strict mode

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,12 @@
-"use strict";
+'use strict';
+//lecture 29: Strict mode
+
+let hasDriversLicense = false;
+let passTest = true;
+
+if(passTest) hasDriversLicense = true;
+if(hasDriversLicense) console.log("I can drive :D");
+
+/* const interface = 'Audio';
+const private = 534;
+ */
