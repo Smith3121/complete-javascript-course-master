57b3a3ca7b01065d6b96736786d5e0e25539c3f9
smith1311 <kovacsakos961@gmail.com>
2023-12-21 21:39:39 +0200

    lecture 30: Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,4 @@
-'use strict';
+/* 'use strict';
 //lecture 29: Strict mode
 
 let hasDriversLicense = false;
@@ -7,6 +7,28 @@ let passTest = true;
 if(passTest) hasDriversLicense = true;
 if(hasDriversLicense) console.log("I can drive :D");
 
-/* const interface = 'Audio';
-const private = 534;
+//const interface = 'Audio';
+//const private = 534;
  */
+ 
+//lecture 30: Functions
+
+function logger() {
+    console.log('My name is Jonas');
+}
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+const appleJuice =  fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
