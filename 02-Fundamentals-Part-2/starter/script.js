ff0e3e96bd0e67d4348191591b24a895765e00bc
smith1311 <kovacsakos961@gmail.com>
2023-12-23 21:08:40 +0200

    lecture 31: Function Declarations vs Expressions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -12,7 +12,7 @@ if(hasDriversLicense) console.log("I can drive :D");
  */
  
 //lecture 30: Functions
-
+/* 
 function logger() {
     console.log('My name is Jonas');
 }
@@ -32,3 +32,19 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 const num = Number('23');
+*/
+
+// lecture 31: Function Declarations vs Expressions
+
+function calcAge1(birthYear){
+    return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+
+const calcAge2 = function (birthYear){
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
