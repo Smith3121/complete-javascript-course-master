a32a3dc42879cea999da4a14be87c85a2c8dcdbc
smith1311 <kovacsakos961@gmail.com>
2023-12-26 17:13:56 +0200

    lecture 36: introduction to arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -140,7 +140,7 @@ To calculate average of 3 values, add them all together and divide by 3
 To check if number A is at least double number B, check for A ▶= 2 * B.
 Apply this to the team's average scores 😉
  */
-
+/* 
 const calcAverage = (scoreOne, scoreTwo, scoreThree) => {
     const avg = (scoreOne + scoreTwo + scoreThree) / 3;
     return avg;
@@ -171,3 +171,44 @@ secondDataD = calcAverage(85, 54, 41);
 secondDataK = calcAverage(23, 34, 27);
 checkWinner(firstDataD, firstDataK);
 checkWinner(secondDataD, secondDataK);
+ */
+
+// lecture 36: introduction to arrays
+
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[1]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+// friends = ["Bob", "Alice"];
+
+const firstName = 'Jonas';
+const jonas = [firstName, 'Schmedtmann', 2037 - 1991, "teacher", friends];
+console.log(jonas);
+
+// Exercise
+const calcAge = function(birthYear){
+    return 2037 - birthYear;
+}
+const years = [1990, 1967, 2002, 2010, 2018];
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
+
