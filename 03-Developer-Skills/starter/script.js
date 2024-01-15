da0e377388eb2648472f34504ba0722269a65748
smith1311 <kovacsakos961@gmail.com>
2024-01-15 22:28:04 +0200

    Setting up Prettier and VS Code | JavaScript 🔥 | Lecture 050

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,11 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+// Setting up Prettier and VS Code | JavaScript 🔥 | Lecture 050
+
+const x = 23;
+if (x === 23) console.log(23);
+
+const calcAge = birthYear => 2037 - birthYear;
+
+console.log(x);
