94348b942ceb74c7c4a7a00e3ed40f58026e68c5
Kovács Ákos <kovacsakos961@gmail.com>
2024-01-28 12:55:39 +0200

    Coding Challenge #9 | JavaScript 🔥 | Lecture 057

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -74,7 +74,7 @@ const calcTempAmplitudeNew = function (t1, t2) {
 };
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 console.log(amplitudeNew);
-*/
+
 
 // Debugging with the Console and Breakpoints | JavaScript 🔥 | Lecture 056
 
@@ -122,3 +122,34 @@ const calcTempAmplitudeBug = function (t1, t2) {
 const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
 // A) Identify
 console.log(amplitudeBug);
+*/
+
+// Coding Challenge #9 | JavaScript 🔥 | Lecture 057
+// Given an array of forecasted maximum temperatures, the thermometer displays a string with the given temperatures.
+// Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+// Your tasks:
+// 1. Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+// Try it with both test datasets.
+// 2. Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+// Test data:
+// Data 1: [17, 21, 23]
+// Data 2: [12, 5, -5, 0, 4]
+
+// GOOD LUCK 😀
+
+const testDataOne = [17, 21, 23];
+const testDataTwo = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+  let stringToPrint = "";
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    stringToPrint = stringToPrint + `... ${element}ºC in ${i + 1} days ...`;
+  }
+  console.log(stringToPrint);
+};
+
+printForecast(testDataOne);
+printForecast(testDataTwo);
