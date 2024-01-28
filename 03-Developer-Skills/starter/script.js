b1004658d1fed518c5a44f46d03d7ffa25eb6ef0
Kovács Ákos <kovacsakos961@gmail.com>
2024-01-28 12:28:40 +0200

    Debugging with the Console and Breakpoints | JavaScript 🔥 | Lecture 056

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -9,7 +9,7 @@ if (x === 23) console.log(23);
 const calcAge = birthYear => 2037 - birthYear;
 
 console.log(calcAge(1971));
-*/
+
 
 // Using Google, StackOverflow and MDN
 
@@ -74,3 +74,51 @@ const calcTempAmplitudeNew = function (t1, t2) {
 };
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 console.log(amplitudeNew);
+*/
+
+// Debugging with the Console and Breakpoints | JavaScript 🔥 | Lecture 056
+
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+
+    // C) Fix
+    // value: Number(prompt("Degree ceclsius:")),
+    value: 10,
+  };
+
+  // B) Find
+  console.table(measurement);
+
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A) Identify the bug
+console.log(measureKelvin());
+
+// Using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+
+  debugger;
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// A) Identify
+console.log(amplitudeBug);
