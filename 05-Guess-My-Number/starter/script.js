ad6980981b63810a84eda63bea4fd4f4eceec6f8
Kovács Ákos <kovacsakos961@gmail.com>
2024-01-29 19:02:33 +0200

    Handling Click Events | JavaScript 🔥 | Lecture 067

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,4 +1,5 @@
 'use strict';
+/*
 // PROJECT #1_ Guess My Number!. | JavaScript 🔥 | Lecture 064
 
 console.log(document.querySelector('.message').textContent);
@@ -11,3 +12,13 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
+});
