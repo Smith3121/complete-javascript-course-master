2843c9e834a1482e03917d5e6b23a88be25cd3a1
Kovács Ákos <kovacsakos961@gmail.com>
2024-01-29 18:43:54 +0200

    Selecting and Manipulating Elements | JavaScript 🔥 | Lecture 066

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,3 +2,12 @@
 // PROJECT #1_ Guess My Number!. | JavaScript 🔥 | Lecture 064
 
 console.log(document.querySelector('.message').textContent);
+
+//Selecting and Manipulating Elements | JavaScript 🔥 | Lecture 066
+
+document.querySelector('.message').textContent = 'Correct Number!';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
