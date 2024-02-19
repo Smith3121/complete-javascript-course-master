50a1b01e498bb26d5c6e0dae7b4f360cec1163e5
Kovács Ákos <kovacsakos961@gmail.com>
2024-02-19 19:08:38 +0200

    The this Keyword in Practice | JavaScript 🔥 | Lecture 090

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -40,7 +40,7 @@ const firstName = 'Jonas';
 calcAge(1991);
 // console.log(age);
 // printAge();
-*/
+
 
 ///////////////////////////////////////
 // Hoisting and TDZ in Practice
@@ -87,3 +87,39 @@ const z = 3;
 console.log(x === window.x);
 console.log(y === window.y);
 console.log(z === window.z);
+*/
+
+///////////////////////////////////////
+// The this Keyword in Practice
+console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();
