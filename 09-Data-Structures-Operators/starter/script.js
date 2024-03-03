8dd761b17a84c8a2eea872e993602eef66a3283f
Kovács Ákos <kovacsakos961@gmail.com>
2024-03-03 16:49:06 +0200

    The Nullish Coalescing Operator (__) | JavaScript 🔥 | Lecture 100

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -51,6 +51,17 @@ const restaurant = {
   },
 };
 
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+
+/*
+///////////////////////////////////////////
+// Shprt Circuiting (&& and ||)
 console.log('-------- OR --------');
 
 // Use ANY data type, return ANY data type, short-circuiting
@@ -80,6 +91,7 @@ if (restaurant.orderPizza) {
 }
 
 restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+*/
 
 /*
 /////////////////////////////////////
