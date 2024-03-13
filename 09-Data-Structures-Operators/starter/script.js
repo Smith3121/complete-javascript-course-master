9e4a8cb7ad13f6c1fcf728670498cd7659234e1f
Kovács Ákos <kovacsakos961@gmail.com>
2024-03-13 20:46:22 +0200

    Coding Challenge #11 | JavaScript 🔥 | Lecture 102

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -80,6 +80,7 @@ rest2.owner &&= '<ANONYMUS>';
 
 console.log(rest1);
 console.log(rest2);
+*/
 
 /*
 /////////////////////////////////////////////////////
@@ -297,3 +298,112 @@ console.log(i, j, k);
 const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r);
 */
+
+//////////////////////////////
+// Coding Challenge #1
+// We're building a football betting app (soccer for my American friends 😅)!
+// Suppose we get data from a web service about a certain game ('game' variable on
+// next page). In this challenge we're gonna work with that data.
+// Your tasks:
+// 1. Create one player array for each team (variables 'players1' and
+// 'players2')
+// 2. The first player in any player array is the goalkeeper and the others are field
+// players. For Bayern Munich (team 1) create one variable ('gk') with the
+// goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
+// field players
+// 3. Create an array 'allPlayers' containing all players of both teams (22
+// players)
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
+// new array ('players1Final') containing all the original team1 players plus
+// 'Thiago', 'Coutinho' and 'Perisic'
+// 5. Based on the game.odds object, create one variable for each odd (called
+// 'team1', 'draw' and 'team2')
+// 6. Write a function ('printGoals') that receives an arbitrary number of player
+// names (not an array) and prints each of them to the console, along with the
+// number of goals that were scored in total (number of player names passed in)
+// 7. The team with the lower odd is more likely to win. Print to the console which
+// team is more likely to win, without using an if/else statement or the ternary
+// operator.
+// Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
+// Then, call the function again with players from game.scored
+// GOOD LUCK 😀
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const [bayernTeam, dortmundTeam] = game.players;
+console.log('players: ', game.players);
+
+console.log('bayernTeam', bayernTeam);
+
+const players1 = [bayernTeam];
+console.log('players1: ', players1);
+
+const gk = bayernTeam[0];
+console.log('goalKeeper: ', gk);
+
+const fieldPlayers = [];
+const allPlayers = [...bayernTeam, ...dortmundTeam];
+console.log('allPlayers', allPlayers);
+
+const players1Final = [...bayernTeam, 'Thiago', 'Coutinho', 'Perisic'];
+console.log('players1Final: ', players1Final);
+
+const players2 = [dortmundTeam];
+console.log('dortmundTeam: ', dortmundTeam);
+
+const team1 = game.odds.team1;
+const draw = game.odds.x;
+const team2 = game.odds.team2;
+
+console.log('Odds of team1, draw, team2: ', team1, draw, team2);
+
+const printGoals = function (...players) {
+  let sum = 0;
+  for (let i = 0; i < players.length; i++) {
+    sum += 1;
+    console.log(players[i]);
+  }
+  console.log(sum);
+};
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals('game.scored call: ', ...game.scored);
