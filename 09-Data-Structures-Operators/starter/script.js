cbc71f6ff9f24e4daeca008f1f6420e93324a609
Kovács Ákos <kovacsakos961@gmail.com>
2024-05-05 16:32:12 +0300

    Coding Challenge #12 | JavaScript 🔥 | Lecture 107

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -91,6 +91,8 @@ console.log(users[0]?.name ?? 'User array empty');
 // if (users.length > 0) console.log(users[0].name);
 // else console.log('user array empty');
 */
+
+/*
 ///////////////////////////////////////////////////
 // Looping Objects_ Object Keys, Values, and Entries | JavaScript 🔥 | Lecture 106
 
@@ -116,6 +118,7 @@ console.log(entries);
 for (const [day, { open, close }] of entries) {
   console.log(`On ${day} we open at ${open} and close at ${close}`);
 }
+*/
 
 /*
 //////////////////////////////
@@ -517,3 +520,118 @@ printGoals('game.scored call: ', ...game.scored);
 team1 < team2 && console.log('Bayern Munich is more likely to win');
 team1 > team2 && console.log('Borrussia Dortmund is more likely to win');
 */
+
+// LECTURE: 107
+
+// Coding Challenge #12 | JavaScript 🔥 | Lecture 107
+// Let's continue with our football betting app! Keep using the 'game' variable from before.
+
+// Your tasks:
+// 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski").
+// 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember).
+// 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+// Odd of victory Bayern Munich: 1.33
+// Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them
+// (except for "draw").
+
+// Hint: Note how the odds and the game objects have the same property names 😉
+
+// 4. Bonus: Create an object called 'scorers' which contains the names of the
+// players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+// {
+// Gnarby: 1,
+// Hummels: 1,
+// Lewandowski: 2
+// }
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1.
+for (let index = 0; index < game.scored.length; index++) {
+  console.log(`Goal ${index + 1}: ${game.scored[index]}`);
+}
+// nicer solution
+// for (const [i, player] of game.scored.entries())
+//  console.log(`Goal ${i + 1}: ${player}`);
+
+// 2.
+let totalOdds = 0;
+let avg;
+for (let property in game.odds) {
+  totalOdds += game.odds[property];
+}
+avg = totalOdds / Object.keys(game.odds).length;
+console.log('Average odds:', avg);
+// nicer way
+// const odds = Object.values(game.odds);
+// let average = 0;
+// for (const odd of odds) average += odd;
+// average /= odds.length;
+// console.log(average);
+
+// 3.
+console.log(
+  `Odd of victory ${game.team1}: ${game.odds.team1}. Odd of draw: ${game.odds.x}. Odd of victory ${game.team2}: ${game.odds.team2}.`
+);
+// for (const [team, odd] of Object.entries(game.odds)) {
+//   const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+//   console.log(`Odd of ${teamStr} ${odd}`);
+// }
+
+const scorers = {};
+
+// 4.
+for (let player of game.scored) {
+  if (scorers[player]) {
+    scorers[player]++;
+  } else {
+    scorers[player] = 1;
+  }
+}
+console.log(scorers);
+
+// const scorers = {};
+// for (const player of game.scored) {
+//   scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+// }
