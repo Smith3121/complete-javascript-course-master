9bec9031c847aa88eedca028160a4119158ce7bc
Kovács Ákos <kovacsakos961@gmail.com>
2024-05-06 15:13:56 +0300

    Maps: Fundamentals | JavaScript 🔥 | Lecture 109

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -637,6 +637,8 @@ console.log(scorers);
 //   scorers[player] ? scorers[player]++ : (scorers[player] = 1);
 // }
 */
+
+/*
 ///////////////////////////////////////////////////////
 // Sets | JavaScript 🔥 | Lecture 108
 
@@ -674,3 +676,38 @@ console.log(
 );
 
 console.log(new Set('jonasschmedtmann').size);
+*/
+
+///////////////////////////////////////////////
+// Maps: Fundamentals | JavaScript 🔥 | Lecture 109
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+
+const arr = [1, 2];
+rest.set(document.querySelector('h1'), 'Heading');
+rest.set(arr, 'Test');
+console.log(rest);
+console.log(rest.size);
+// rest.clear()
+
+console.log(rest.get(arr));
