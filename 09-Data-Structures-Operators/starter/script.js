0c6e831378c4a33c9944cfad70dff4eade1ef524
Kovács Ákos <kovacsakos961@gmail.com>
2024-05-07 19:46:21 +0300

    Coding Challenge #13 | JavaScript 🔥 | Lecture 112

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -714,6 +714,7 @@ console.log(rest.size);
 console.log(rest.get(arr));
 */
 
+/*
 ////////////////////////////////////////////////////
 // Maps: Iteration | JavaScript 🔥 | Lecture 110
 
@@ -749,3 +750,70 @@ console.log(...question);
 // console.log(question.entries());
 console.log(...question.keys());
 console.log(...question.values());
+*/
+
+///////////////////////////////
+// Coding Challenge #13 | JavaScript 🔥 | Lecture 112
+
+// Let's continue with our football betting app! This time, we have a map called 'gameEvents' (see below) with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+// Your tasks:
+// 1. Create an array 'events' of the different game events that happened (no
+// duplicates)
+
+// 2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+
+// 3. Compute and log the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+
+// 4. Loop over 'gameEvents' and log each element to the console, marking
+// whether it's in the first half or second half (after 45 min) of the game, like this:   [FIRST HALF] 17: ⚽ GOAL
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// 1.
+const tempSet = new Set();
+console.log(tempSet);
+
+for (const [key, value] of gameEvents) {
+  tempSet.add(value);
+}
+const events = [...tempSet];
+console.log(tempSet);
+console.log(events);
+
+// better way
+// const events = [...new Set(gameEvents.values())];
+// console.log(events);
+
+// 2.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3.
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+// 4.
+for (const [min, event] of gameEvents) {
+  if (min <= 45) console.log(`[FIRST HALF] ${min}: ${event}`);
+  else console.log(`[SECOND HALF] ${min}: ${event}`);
+}
+
+// Slightly different implementation
+// for (const [min, event] of gameEvents) {
+//   const half = min <= 45 ? 'FIRST' : 'SECOND';
+//   console.log(`[${half} HALF] ${min}: ${event}`);
+// }
