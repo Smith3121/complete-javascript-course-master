46b3ea49d84aaeaa2d4d6aec1a1c70c98b57a905
Kovács Ákos <kovacsakos961@gmail.com>
2024-05-07 20:04:56 +0300

    Working With Strings - Part 1 | JavaScript 🔥 | Lecture 113

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -752,6 +752,7 @@ console.log(...question.keys());
 console.log(...question.values());
 */
 
+/*
 ///////////////////////////////
 // Coding Challenge #13 | JavaScript 🔥 | Lecture 112
 
@@ -817,3 +818,47 @@ for (const [min, event] of gameEvents) {
 //   const half = min <= 45 ? 'FIRST' : 'SECOND';
 //   console.log(`[${half} HALF] ${min}: ${event}`);
 // }
+*/
+
+//////////////////////////////
+// Working With Strings - Part 1 | JavaScript 🔥 | Lecture 113
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are the middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') console.log('You got the middle seat :|');
+  else console.log('You got lucky :D');
+};
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+console.log(new String('Jonas'));
+console.log(typeof new String('Jonas'));
+
+console.log(typeof new String('Jonas').slice(1));
