044a54807682cc2baddf5db5e6735458e8dc9c6e
Smith3121 <58434014+Smith3121@users.noreply.github.com>
2024-06-01 08:49:02 +0000

    Working With Strings - Part 3 | JavaScript 🔥 | Lecture 115

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -866,6 +866,8 @@ console.log(typeof new String('Jonas'));
 console.log(typeof new String('Jonas').slice(1));
 */
 
+/*
+
 /////////////////////////////////////////
 // Working With Strings - Part 2 | JavaScript 🔥 | Lecture 114
 
@@ -926,3 +928,59 @@ const checkBaggage = function (items) {
 checkBaggage('I have a laptop, some food and a pocket Knife');
 checkBaggage('I have some socks and camera');
 checkBaggage('Got some snacks and a gun for protection');
+
+*/
+////////////////////////////////////////////
+// Working With Strings - Part 3 | JavaScript 🔥 | Lecture 115
+
+// Split and join
+console.log('a+very+nice+string'.split('+'));
+console.log('Jonas Schmedtmann'.split(' '));
+
+const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ');
+
+const newname = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newname);
+
+const capitalizeName = function(name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for (const n of names) {
+// namesUpper.push(n[0].toUpperCase() + n.slice(1))
+   namesUpper.push(n.replace(n[0], n[0].toUpperCase()))
+
+  }
+  console.log(namesUpper.join(' '));
+}
+
+capitalizeName('jessica and smith davis')
+capitalizeName('jonas schmedtmann')
+
+// Padding strings
+
+const message = 'Go to gate 23!';
+console.log(message.padStart(25, '+').padEnd(30, '+'));
+console.log('Jonas'.padStart(25, '+').padEnd(30, '+'));;
+
+const maskCreditCrad = function(number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*')
+}
+
+console.log(maskCreditCrad(4333342321343444334324243234))
+console.log(maskCreditCrad('454532455466562555000493988292498276625344545')
+);  
+
+// Repeat
+const message2 = 'Bad weather... All Departures Delayed...';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'I'.repeat(n)}`);
+}
+
+planesInLine(5)
+planesInLine(3)
+planesInLine(12)
