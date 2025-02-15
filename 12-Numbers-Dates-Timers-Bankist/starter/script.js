c25b5b3e0809fcccb5354839c9949c37b6f9f934
Kovács Ákos <kovacsakos961@gmail.com>
2025-02-15 11:06:04 +0200

    181. The Remainder Operator

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -290,6 +290,7 @@ console.log(Number.isInteger(23.0));
 console.log(Number.isInteger(23 / 0));
 */
 
+/*
 // 180. Math and Rounding
 
 console.log(Math.sqrt(25));
@@ -332,3 +333,27 @@ console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log((2.345).toFixed(2));
 console.log(+(2.345).toFixed(2));
+*/
+
+// 181. The Remainder Operator
+
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
