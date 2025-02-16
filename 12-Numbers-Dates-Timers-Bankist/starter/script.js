cdfa344efc444febeeb27eee66cb2835bb5dcc2d
Kovács Ákos <kovacsakos961@gmail.com>
2025-02-16 10:57:26 +0200

    184. Creating Dates

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -380,6 +380,7 @@ console.log(Number('230_000'));
 console.log(parseInt('230_000'));
 */
 
+/*
 // 183. Working with BigInt
 
 console.log(2 ** 53 - 1);
@@ -413,3 +414,41 @@ console.log(huge + ' is Really big!!!');
 // Division
 console.log(11n / 3n);
 console.log(10 / 3);
+*/
+
+/*
+// 184. Creating Dates
+const now = new Date();
+console.log(now);
+
+console.log(new Date('Feb 16 2025 10:06:12'));
+console.log(new Date('December 24, 2024'));
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 31));
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+
+// Working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142249780000));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
+*/
