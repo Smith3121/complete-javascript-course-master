9e208a790a542716e46dd88a41b669eb81a3f26e
smith1311 <kovacsakos961@gmail.com>
2025-03-08 13:33:27 +0200

    210. Lazy Loading Images

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -198,6 +198,34 @@ allSections.forEach(function (section) {
   section.classList.add('section--hidden');
 });
 
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+console.log(imgTargets);
+
+const loadImg = function (entries, observer){
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function(){
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+}
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+ rootMargin: '200px',
+})
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
 // tabs.forEach(t => t.addEventListener('click', () => console.log('TAB')));
 /*
 // Selecting, Creating and Deleting Elements
