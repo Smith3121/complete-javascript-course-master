99a801948bef93fde2ffadcdf417b68778435f36
smith1311 <kovacsakos961@gmail.com>
2025-03-08 15:10:09 +0200

    212. Building a Slider Component: Part 2

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -227,23 +227,33 @@ const imgObserver = new IntersectionObserver(loadImg, {
 imgTargets.forEach(img => imgObserver.observe(img));
 
 // Slider
+const slider = function(){
+
 const slides = document.querySelectorAll('.slide');
 const btnLeft = document.querySelector('.slider__btn--left');
 const btnRight = document.querySelector('.slider__btn--right');
+const dotContainer = document.querySelector('.dots');
 
 let curSlide = 0;
 const maxSlide = slides.length;
 
-// const slider = document.querySelector('.slider');
-// slider.style.transform = 'scale(0.4) translateX(-800px)';
-// slider.style.overflow = 'visible';
+// Slider functions
+const createDots = function(){
+  slides.forEach(function(_, i){
+    dotContainer.insertAdjacentHTML('beforeend', `<button class="dots__dot" data-slide="${i}"></button>`);
+  });
+};
+
+
+const activateDot = function(slide){
+  document.querySelectorAll('.dots__dot').forEach(dot => dot.classList.remove('dots__dot--active'));
+  document.querySelector(`.dots__dot[data-slide="${slide}"]`).classList.add('dots__dot--active');
+};
 
 
 const goToSlide = function(slide){
   slides.forEach((s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`));
 }
-
-goToSlide(0);
 
 // Next slide
 const nextSlide = function(){
@@ -253,6 +263,7 @@ const nextSlide = function(){
     curSlide++;
   }
   goToSlide(curSlide);
+  activateDot(curSlide);
 }
 
 const prevSlide = function(){
@@ -262,11 +273,35 @@ const prevSlide = function(){
   curSlide--;
 }
 goToSlide(curSlide);
+activateDot(curSlide);
 }
 
+const init = function(){
+  goToSlide(0);
+  createDots();
+  activateDot(0);
+}
+init();
 
+
+// Event handlers
 btnRight.addEventListener('click', nextSlide);
 btnLeft.addEventListener('click', prevSlide)
+
+document.addEventListener('keydown', function(e){
+  if(e.key === 'ArrowLeft') prevSlide();
+  e.key === 'ArrowRight' && nextSlide();
+})
+
+dotContainer.addEventListener('click', function(e){
+  if(e.target.classList.contains('dots__dot')){
+    curSlide = Number(e.target.dataset.slide);
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  }
+})
+}
+slider();
 
 // tabs.forEach(t => t.addEventListener('click', () => console.log('TAB')));
 /*
