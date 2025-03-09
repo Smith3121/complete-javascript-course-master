3bc2d57874769343672552686303ba0a775c37d0
smith1311 <kovacsakos961@gmail.com>
2025-03-09 11:44:49 +0200

    Coding Challenge #1

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 const Person = function (firstName, birthYear) {
     // Instance properties
     this.firstName = firstName;
@@ -69,3 +69,48 @@ console.log(arr.unique());
 
 const h1 = document.querySelector('h1')
 console.dir(x => x + 1)
+/*
+
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+const Car = function (make, speed) {
+    // Instance properties
+    this.make = make;
+    this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+    this.speed += 10
+    console.log(this.speed);
+};
+
+Car.prototype.brake = function () {
+    this.speed -= 5
+    console.log(this.speed);
+}
+
+const mazda = new Car('MAZDA', 120);
+const toyota = new Car('TOYOTA', 95);
+
+mazda.accelerate();
+mazda.accelerate();
+mazda.brake();
+mazda.accelerate();
+toyota.accelerate();
+toyota.accelerate();
+toyota.brake();
+toyota.accelerate();
