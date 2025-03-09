aafd361b7c186cc86651427004541c61758c6932
smith1311 <kovacsakos961@gmail.com>
2025-03-09 17:04:37 +0200

    225. Setters and Getter

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -136,11 +136,26 @@ class PersonCl {
     greet() {
         console.log(`Hey ${this.fullName}`);
     }
+
+    get age() {
+        return 2037 - this.birthYear;
+    }
+
+    // Set a property that already exists
+    set fullName(name) {
+        if (name.includes(' ')) this._fullName = name;
+        else alert(`${name} is not a full name!`);
+    }
+
+    get fullName() {
+        return this._fullName;
+    }
 }
 
 const jessica = new PersonCl('Jessica Davis', 1996);
 console.log(jessica);
 jessica.calcAge();
+console.log(jessica.age);
 
 console.log(jessica.__proto__ === PersonCl.prototype);
 
@@ -153,3 +168,23 @@ jessica.greet();
 // 1. Classes are NOT hoisted
 // 2. Classes are first-class citizens
 // 3. Classes are executed in strict mode
+
+const walter = new PersonCl('Walter White', 1965);
+
+const account = {
+    owner: 'Jonas',
+    movements: [200, 530, 120, 300],
+
+    get latest() {
+        return this.movements.slice(-1).pop();
+    },
+
+    set latest(mov) {
+        this.movements.push(mov);
+    }
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
