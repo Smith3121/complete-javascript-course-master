7d24f35eda095722b54c60bf3c1dbe09f9566964
Kovács Ákos <kovacsakos961@gmail.com>
2025-03-12 18:45:37 +0200

    233. Another Class Example

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -465,6 +465,7 @@ martha.introduce();
 martha.calcAge();
 */
 
+/*
 ////////////////////////////////
 // Inheritance Between "Classes": Object.create
 
@@ -495,3 +496,47 @@ const jay = Object.create(StudentProto);
 jay.init('Jay', 2010, 'Computer Science');
 jay.introduce();
 jay.calcAge();
+*/
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public interface
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log('Loan approved');
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+
+console.log(acc1);
+console.log(acc1.pin);
