518f75440656a1f35bff62cf2b2767235f7e7344
Kovács Ákos <kovacsakos961@gmail.com>
2025-03-13 18:03:14 +0200

    237. CHALLENGE #4

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -375,8 +375,7 @@ const EV = function (make, speed, charge) {
   this.charge = charge;
 };
 
-// Linking prototypes
-EV.prototype = Object.create(Car.prototype);
+// No need to link prototypes manually in ES6 classes
 
 EV.prototype.chargeBattery = function (chargeTo) {
   this.charge = chargeTo;
@@ -497,6 +496,7 @@ jay.introduce();
 jay.calcAge();
 */
 
+/*
 // Encapsulation: Protected Properties and Methods
 
 // 1) Public fields
@@ -572,3 +572,73 @@ console.log(acc1);
 // Account.test();
 
 console.log(movements);
+*/
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+1. Re-create challenge #3, but this time using ES6 classes: 
+create an 'EVCl' child class of the 'CarCl' class
+2. Make the 'charge' property private;
+3. Implement the ability to chain the 'accelerate' and 
+'chargeBattery' methods of this class, and also update the 'brake' 
+method in the 'CarCl' class. They experiment with chining!
+
+DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+*/
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} going at ${this.speed} km/h`);
+    return this;
+  }
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} going at ${this.speed} km/h`);
+    return this;
+  }
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+// Child class EV
+class EVCl extends CarCl {
+  #charge;
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+  accelerate() {
+    this.speed += 20;
+    this.#charge--;
+    console.log(
+      `${this.make} going at ${this.speed} km/h, with a charge of ${
+        this.#charge
+      }%`
+    );
+    return this;
+  }
+}
+
+// Creating an instance of EV
+const rivian = new EVCl('Rivian', 120, 23);
+
+console.log(rivian.speedUS);
+rivian.accelerate().brake().chargeBattery(90).accelerate().accelerate();
+console.log(rivian.speedUS);
