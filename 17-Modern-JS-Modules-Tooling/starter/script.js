860f7939b436756bf10bf36c18ba0fec2febe96a
Kovács Ákos <kovacsakos961@gmail.com>
2025-05-17 12:50:39 +0300

    285. Top-Level await (ES2022)

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -16,3 +16,25 @@ add('pizza', 2);
 add('apple', 4);
 
 console.log(cart);
+
+// console.log('Starting fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// Not very clean, but works
+// lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
--- 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -1,6 +1,11 @@
 // Exporting module
 console.log('Exporting module');
 
+// BLOCKING CODE
+// console.log('Start fetching users');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Finish fetching users');
+
 const shippingCost = 10;
 export const cart = [];
 
